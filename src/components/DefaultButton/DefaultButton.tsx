95881225ecbf880da7404e0da2597fb72fbbebdd
Gonzalo <gon.pineiro@gmail.com>
2021-07-26 21:07:11 -0300

    :hammer: FinishClass

--- src/components/DefaultButton/DefaultButton.tsx
+++ src/components/DefaultButton/DefaultButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, TouchableOpacity, ViewStyle } from 'react-native';
+import { colors } from '../../utils/theme';
 
 import styles from './styles';
 
@@ -9,22 +10,27 @@ interface Props {
   msg: string;
   onPress: () => void;
   textSize: number;
+  variant?: 'primary' | 'secondary';
 }
-
-const DefaultButton = ({ additionalStyle, color, onPress, msg, textSize }: Props) => {
+/* Primary: backgroundColor oratnte text white */
+/* Primary: backgroundColor white text orange */
+const DefaultButton = ({ additionalStyle, color, onPress, msg, textSize, variant }: Props) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       style={[styles.mainContainer, additionalStyle, { backgroundColor: color }]}
     >
-      <Text style={{ fontSize: textSize }}>{msg}</Text>
+      <Text style={{ fontSize: textSize }}>{msg + variant}</Text>
     </TouchableOpacity>
   );
 };
 
 DefaultButton.defaultProps = {
   additionalStyle: {},
+  color: colors.mainOrange,
   msg: 'Press me',
+  textSize: 14,
+  variant: 'primary',
 };
 
 export default DefaultButton;
