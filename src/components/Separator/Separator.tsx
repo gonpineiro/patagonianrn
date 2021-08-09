82ba737a7052eee2036b63b0102dfd8ef87deaae
Gonzalo <gon.pineiro@gmail.com>
2021-08-09 19:23:05 -0300

    :hammer: Separetor

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -4,31 +4,29 @@ import { colors } from '../../utils/theme';
 import { DEVICE_WIDTH } from '../../utils/dimensions';
 
 interface Props {
-  marginTop?: number;
-  marginBottom?: number;
+  margin?: number;
   color?: string;
   borderBottomWidth?: number;
   width?: number;
 }
 
-const getStyle = ({ borderBottomWidth, color, marginBottom, marginTop, width = 50 }: Props) => {
+const getStyle = ({ borderBottomWidth, color, margin, width = 50 }: Props) => {
   return {
     borderBottomWidth,
     borderBottomColor: color,
-    marginBottom,
-    marginTop,
+    marginBottom: margin,
+    marginTop: margin,
     width: DEVICE_WIDTH - width,
   };
 };
 
-const Separator = ({ marginTop, marginBottom, color, borderBottomWidth, width }: Props) => {
-  const style = getStyle({ marginTop, marginBottom, color, borderBottomWidth, width });
+const Separator = ({ margin, color, borderBottomWidth, width }: Props) => {
+  const style = getStyle({ margin, color, borderBottomWidth, width });
   return <View style={style} />;
 };
 
 Separator.defaultProps = {
-  marginTop: 20,
-  marginBottom: 20,
+  margin: 20,
   borderBottomWidth: 1,
   color: colors.white,
   width: DEVICE_WIDTH - 50,
